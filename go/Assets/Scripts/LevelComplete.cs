74520b1ad306893ee595b6b25875934b8c2db19d
Augiiz <augis916@gmail.com>
2020-04-25 19:29:48 +0300

    v1

---
+++ go/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class LevelComplete : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SaveFuckingLevelLmao()
+    {
+        File.Delete(Application.persistentDataPath + "/gay.xd");
+        using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(Application.persistentDataPath + "/gay.xd", true))
+        {
+            int levelL0L = SceneManager.GetActiveScene().buildIndex;
+            file.WriteLine(levelL0L);
+        }
+    }
+    public void BACKTHEFUCKOFF()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ go/Assets/loadMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+
+public class loadMenu : MonoBehaviour
+{
+
+    public void loadGame()
+    {
+        string line;
+        if (File.Exists(Application.persistentDataPath + "/gay.xd"))
+        {
+            using (StreamReader sr = new StreamReader(Application.persistentDataPath + "/gay.xd"))
+            {
+                line = sr.ReadLine();
+            }
+            SceneManager.LoadScene(Convert.ToInt32(line) + 1);
+        }
+    }
+
+
+
+    public void deleteLoadFile()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gay.xd")) 
+        {
+            File.Delete(Application.persistentDataPath + "/gay.xd");
+        }       
+    }
+
+
+}
