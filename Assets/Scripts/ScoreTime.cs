2750e88791a0055004f65f43588668a0cc361b3e
Augiiz <augis916@gmail.com>
2020-04-26 17:09:47 +0300

    v1.12
    prod by m'torka

---
+++ Assets/Scripts/ScoreTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTime : MonoBehaviour
+{
+    int timer;
+
+    public Text scoreText;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Convert.ToInt32(Time.deltaTime);
+        scoreText.text = timer.ToString();
+    }
+}
